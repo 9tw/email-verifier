f5712cbbf4669740da3a0bdde304b41b8e2abdc0
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2023-12-27 18:30:19 +0900

    initial commit

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	// cfg := config.NewConfig()
+	// db := database.InitDB(cfg)
+	// database.MigrateDB(db)
+
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
